23928c572016fa12dc0722ed679bde295e3d1c51
Phillip Jackson <unisys12@gmail.com>
2016-07-24 09:41:05 -0500

    remove conflict markers

--- bot.js
+++ bot.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-// Import dependencies and Utilities
-const dischord = require('discord.js');
-var config = require('./bot_core/config.js');
-var utilities = require('./bot_core/utils.js');
-
-// Instantiate classes
-var bot = new dischord.Client();
-
-// Authenticate with Discord Chat server using TOKEN
-=======
 const discord = require('discord.js');
 var bot = new discord.Client();
 
@@ -20,7 +9,6 @@ var config = require('./config.js');
 require('./server.js');
 
 // Authenticate with the server using a TOKEN
->>>>>>> auth
 bot.loginWithToken(config.BOT_TOKEN, function (token, err) {
     // If Error, display it
     if(err){
@@ -28,36 +16,18 @@ bot.loginWithToken(config.BOT_TOKEN, function (token, err) {
     }
     // On the event of a message begin sent grab the message object
     bot.on('message', function (message) {
-<<<<<<< HEAD
         
         // Process when a chat message is posted        
-=======
->>>>>>> auth
         // look at the content of the message
         var input = message.content;
         // check for the cmd at the beginning of the message content
         var logCmd = input.startsWith('!logs');
-<<<<<<< HEAD
+        
         // if found, send a DM to the author that gave the cmd
-=======
-        // if found, send a message to the author that gave the cmd
->>>>>>> auth
         if (logCmd) { 
             bot.sendMessage(message.author, "To download the latest Message Logs, http://ffcdiscordbot.app/logs");
         }
-
-<<<<<<< HEAD
-        //Chat Logging - Make sure not to capture DMs. 
-        if (!message.channel.name) {
-            return;
-        } else {
-            utilities.logMessage(message);
-        }
-
-    });
-   
-});
-=======
+        
         //Chat Logging
         if (!message.channel.name) {
             return;
@@ -68,4 +38,3 @@ bot.loginWithToken(config.BOT_TOKEN, function (token, err) {
     });
    
 });
->>>>>>> auth
